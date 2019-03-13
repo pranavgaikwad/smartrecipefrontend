135ffc85de79703ec3e028559d98c8f903782a0e
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-03-13 17:37:01 -0400

    a63684c added reducer for ingredients 7c6a042 created form for basic recipe information editing 1a5ce3a added dialog and form for adding / editing recipes a29bb81 added some more actions for saving recipes ab4c3d9 added actions and reducers for ingredient related actions 1d25f5e updated packages 6fbd9ee added constants for recipe related actions 798d6d8 added recipesReducer 35ed869 added default schema representations in initial state of ingredients' store c501925 minor changes 444141a added dynamically created recipes on Recipes landing page, added create new recipe dialog bdd95e5 added dynamically created grid, connected it with redux store 21ab716 changed default card component to accept Recipe information 56e4551 minor changes in ingredients card component a49c0d3 added redux uuid and thunk as middlewares 142cba0 added redux-orm as dependency a61eac2 added stubs for in-memory ORM styled models for the recipes and ingredients cff4b4c added card component to show ingredient information 69c3c06 updated packages ce5e6e1 updated configureStore to use persist redux b85baef added persist store to be used as the main reducer 5c5746c added redux persist store to save redux state on local storage bfe3b8e added fab button to add new recipes commented out unused code 5daf7e1 removed unused import

--- src/containers/ingredients-page/ingredients-page.component.js
+++ src/containers/ingredients-page/ingredients-page.component.js
@@ -1,16 +1,114 @@
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Fab from '@material-ui/core/Fab';
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
+
+import { addIngredient } from '../../actions/ingredients-page/ingredients-page.actions';
+
+import IngredientCardComponent from '../../components/cards/ingredient-card.component';
+
+const GRID_ROW_SIZE = 5;
 
 /**
  * Main container for ingredients page
  */
 class IngredientsPageContainer extends Component {
+  constructor(props){
+    super(props);
+
+    this.onAddNewIngredient = this.onAddNewIngredient.bind(this);
+  }
+
+  onAddNewIngredient() {
+    this.props.addIngredient();
+  }
+
+  onIngredientDeleted() {
+    
+  }
+
+  getIngredientsGrid(ingredients) {
+    const totalIngredients = ingredients.length;
+
+    const totalRows = totalIngredients / GRID_ROW_SIZE;
+
+    let rows = []
+
+    for (var i = 0; i < totalRows; i++) {
+      const start = i*GRID_ROW_SIZE;
+      let end = (i+1)*GRID_ROW_SIZE;
+      end = (totalIngredients < end) ? totalIngredients : end; 
+      let cols = []
+      for (var j = i*GRID_ROW_SIZE; j < end; j++) {
+        const currentIngredient = ingredients[j];
+        if ((currentIngredient !== undefined) && (currentIngredient !== null)) {
+          cols.push(
+            <Grid key={j} item xs>
+              <IngredientCardComponent
+                title={currentIngredient.name}
+              />
+            </Grid>);
+        }
+      }
+      rows.push(<Grid key={"ing_row_" + i} container spacing={8}>{cols}</Grid>);
+    }
+
+    return rows;
+  }
+
   render() {
+    const { classes, ingredients } = this.props;
+
+    const ingredientsGrid = this.getIngredientsGrid(ingredients);
+
     return (
-        <div>
-            <h1>Ingredients</h1>
+        <div className="card-deck-container">
+          <Fab onClick={this.onAddNewIngredient} color="primary" aria-label="Add" className={classes.fab}>
+            <AddIcon />
+          </Fab>
+          <div>
+            {
+              ingredientsGrid
+            }
+          </div>
         </div>
     );
   }
 }
 
-export default IngredientsPageContainer;
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  fab: {
+    margin: theme.spacing.unit,
+    position: 'fixed',
+    bottom: 0,
+    right: 0,
+    margin: 24,
+  },
+});
+
+IngredientsPageContainer.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  ingredients: state.ingredientsReducer.ingredients,
+  currentRoute: state.navigationReducer.currentRoute,
+});
+
+
+const mapDispatchToProps = dispatch => ({
+  addIngredient: () => dispatch(addIngredient()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(IngredientsPageContainer));
