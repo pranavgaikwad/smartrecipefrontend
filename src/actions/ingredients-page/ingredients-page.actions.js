b4afffc331c05a8aa555220ae560cf40fe64fc6c
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-04-14 00:58:13 -0400

    removed unnecessary functions from ingredients actions

--- src/actions/ingredients-page/ingredients-page.actions.js
+++ src/actions/ingredients-page/ingredients-page.actions.js
@@ -20,8 +20,8 @@ function setRequestFailed() {
  * Populates redux store with ingredients list
  * @param  {[Object]} ingredients List of ingredients
  */
-function get(ingredients) {
-  return { type: actionsIngredients.get, ingredients };
+function update(user) {
+  return { type: actionsIngredients.update, user };
 }
 
 /**
@@ -29,76 +29,21 @@ function get(ingredients) {
  * @param  {Object} ingredient  Ingredient to add
  * @return 
  */
-export function addIngredient(ingredient={}) {
+export function updateUser(user) {
+  const requestBody = {
+    user: user,
+  }; 
+
   return (dispatch) => {
     dispatch(setRequestPending());
-
-    // if ingredient is edited, then PUT existing ingredient, else POST new ingredient
-    if (ingredient.id) {
-      apiProxy.put(`${apiConstants.baseUrl}${apiConstants.ingredients}${ingredient.id}`, ingredient, '123')
-      .then((response) => {
-        return apiProxy.get(`${apiConstants.baseUrl}${apiConstants.ingredients}`, '123');
-      })
-      .then((response) => {
-        dispatch(get(response));      
-      })
-      .catch((e) => { // eslint-disable-line
-        dispatch(setRequestFailed());
-        console.log('Error getting ingredients', e);
-      })
-    } else {
-      apiProxy.post(`${apiConstants.baseUrl}${apiConstants.ingredients}`, ingredient, '123')
-      .then((response) => {
-        return apiProxy.get(`${apiConstants.baseUrl}${apiConstants.ingredients}`, '123');
-      })
-      .then((response) => {
-        dispatch(get(response));      
-      })
-      .catch((e) => { // eslint-disable-line
-        dispatch(setRequestFailed());
-        console.log('Error getting ingredients', e);
-      })
-    };
-  };
-}
-
-/**
- * Get list of ingredients
- * @return {[Object]} List of ingredients
- */
-export function getIngredients() {
-  return (dispatch) => {
-    dispatch(setRequestPending());
-
-    apiProxy.get(`${apiConstants.baseUrl}${apiConstants.ingredients}`, '123')
+    apiProxy.post(`${apiConstants.baseUrl}${apiConstants.updateUser}`, requestBody, '123')
     .then((response) => {
-      dispatch(get(response));
+      console.log(response);
+      dispatch(update(response.user));
     })
     .catch((e) => { // eslint-disable-line
       dispatch(setRequestFailed());
-      console.log('Error getting ingredients', e);
-    })
-  };
-}
-
-/**
- * Delete an ingredient
- * @param {Object} ingredient 
- */
-export function deleteIngredient(id) {
-  return (dispatch) => {
-    dispatch(setRequestPending());
-
-    apiProxy.delete(`${apiConstants.baseUrl}${apiConstants.ingredients}/${id}`, '123')
-    .then((response) => {
-      return apiProxy.get(`${apiConstants.baseUrl}${apiConstants.ingredients}`, '123');
-    })
-    .then((response) => {
-      dispatch(get(response));      
-    })
-    .catch((e) => { // eslint-disable-line
-      dispatch(setRequestFailed());
-      console.log('Error deleting ingredient', e);
+      console.log('Error updating ingredient', e);
     })
   };
 }
