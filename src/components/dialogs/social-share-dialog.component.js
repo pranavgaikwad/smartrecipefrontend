7a91c4806832d98213ef47cd02de52cc4664cff0
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-04-18 00:31:39 -0400

    added URL for social share recipe

--- src/components/dialogs/social-share-dialog.component.js
+++ src/components/dialogs/social-share-dialog.component.js
@@ -7,7 +7,9 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import { FacebookShareButton, TwitterShareButton, TwitterIcon, FacebookIcon } from 'react-share';
 
-const shareTitle = 'Check out this amazing recipe!';
+import { appConstants } from '../../utils/app.constants';
+
+const shareTitle = 'Check out this amazing recipe on FeedMe app!';
 
 /**
  * Generic card component to show recipe information
@@ -19,6 +21,12 @@ class SocialShareDialogComponent extends Component {
       recipe,
       onClose, 
     } = this.props;
+
+    let { name } = recipe;
+
+    name = name.split(" ").join("+");
+
+    const url = `${appConstants.appUrl}showcase/${name}`
     
     return (
       <Dialog open={open} aria-labelledby="form-dialog-title">
@@ -26,12 +34,12 @@ class SocialShareDialogComponent extends Component {
         <DialogContent>
           <Grid container spacing={8}>
             <Grid item xs={6} sm={6} md={6} lg={6} xl={6} container>
-              <FacebookShareButton title={shareTitle} url='https://google.com'>
+              <FacebookShareButton title={shareTitle} url={url}>
                 <FacebookIcon size={40} round />
               </FacebookShareButton>
             </Grid>
             <Grid item xs={6} sm={6} md={6} lg={6} xl={6} container>
-              <TwitterShareButton title={shareTitle} url='https://google.com'>
+              <TwitterShareButton title={shareTitle} url={url}>
                 <TwitterIcon size={40} round />
               </TwitterShareButton>
             </Grid>
